89ce8f59a03c24fd796bce0443bc30269d2a6edc
Daria Golub <dgolub@travix.com>
2018-09-16 18:20:25 +0200

    Flight model

--- consumer/src/main/java/au/com/dius/pactworkshop/consumer/Client.java
+++ consumer/src/main/java/au/com/dius/pactworkshop/consumer/Client.java
@@ -4,12 +4,14 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public class Client {
-  public JsonNode loadProviderJson() throws UnirestException {
-    return Unirest.get("http://localhost:8080/provider.json")
-      .queryString("validDate", LocalDateTime.now().toString())
-      .asJson().getBody();
-  }
+    public JsonNode loadProviderJson() throws UnirestException {
+        return Unirest.get("http://localhost:8080/flights")
+                .queryString("flightDate", LocalDate.now().toString())
+                .queryString("originAirport", "AMS")
+                .queryString("destinationAirport", "BCN")
+                .asJson().getBody();
+    }
 }
